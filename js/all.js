fb5714789269c0327fce9e223d68a7ccd29f1144
Michael Tsai <37038363+Mingx94@users.noreply.github.com>
2018-05-18 12:08:29 +0800

    5/18
    1.smoove改成0.2.7
    2.rwd
    3.合併nav連結的js點擊程式碼

--- js/all.js
+++ js/all.js
@@ -1,44 +1,62 @@
 $(window).on('load',function() {
-    $("#preloader").delay(1000).fadeOut("slow");
-    $("body").delay(1200).queue(function (next) {
-        $(this).css("overflow","visible");
+    $('#preloader').delay(1000).fadeOut('slow');
+    $('body').delay(1200).queue(function (next) {
+        $(this).css('overflow','visible');
         next();
     });
 });
+
 $(document).ready(function(){
     $(window).scroll(function (e) {
     if ($(window).scrollTop() <= 0){
-        $(".navbar").addClass("at_top");
+        $('.navbar').addClass('at_top');
     }  else{
-        $(".navbar").removeClass("at_top");
+        $('.navbar').removeClass('at_top');
     }
     });
-    $(".card3D").on("click", function (event) {
-        $(event.currentTarget).toggleClass("flip");
+
+    $('#hamburger').on('click',function(e){
+        e.preventDefault();
+        $('ul').slideToggle();
     });
-    $(".about-click").click(function(){
-        $("html,body").animate({scrollTop:$("#about").offset().top},600);
+    $('ul li a').on('click',function(){
+        if ($(window).outerWidth() < 769){
+            $('ul').slideToggle('fast');
+        }
     });
-    $(".home-click").click(function(){
-        $("html,body").animate({scrollTop:$("#page_home").offset().top},600);
+    $(window).on('resize',function () {
+        // console.log($(window).outerWidth());
+        if ($(window).outerWidth() > 768) {
+            $('.navbar #hamburger').css('display', 'none');
+            $('.navbar ul').css('display','flex');
+        } else {
+            $('.navbar #hamburger').css('display', 'block');
+            $('.navbar ul').css('display','block').hide();
+        }
     });
-    $(".skill-click").click(function(){
-        $("html,body").animate({scrollTop:$("#skill").offset().top},600);
-    });
-    $(".works-click").click(function(){
-        $("html,body").animate({scrollTop:$("#works").offset().top},600);
-    });
-    $(".contact-click").click(function(){
-        $("html,body").animate({scrollTop:$("#contact").offset().top},600);
-    });
-    $("span.scrolldown").click(function(){
-        $("html,body").animate({scrollTop:$("#about").offset().top},600);
+    
+    $('.card3D').on('click', function (e) {
+        $(e.currentTarget).toggleClass('flip');
     });
 
-    $(".container").smoove({
-        offset  : '10%',
-        moveY   : '100px',
-      });
+    $('.navbar a').on('click',function(e){
+        var $attr_href = $(e.currentTarget).attr('href');
+        e.preventDefault(); //return false 、 event.preventDefault() 、 或 event.returnValue = false ，避免 <a> 原先應該做的動作。
+        $('html,body').animate({scrollTop:$($attr_href).offset().top},600)
+        // console.log($attr_href);
+    })
+
+    $('span.scrolldown').click(function(){
+        $('html,body').animate({scrollTop:$('#about').offset().top},600);
+    });
+
+    //smoove.js
+    $('.container').smoove({
+        offset: '5%',
+        moveY: '60%'
+    });
+    //clipboard.js
+    var clipboard = new ClipboardJS('#email');
 });
 
-var clipboard = new ClipboardJS('#email');
+
